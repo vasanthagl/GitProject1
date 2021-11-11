798892388438c2ec906b307575bf321d5df9ca84
vasanthagl <vasantha.glh@gmail.com>
2021-11-11 11:40:23 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace GitProject1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Apples");
+            Console.WriteLine("oranges");
         }
     }
 }
